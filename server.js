817b2c6c534f72f81ebe0d785a6e1b0885bc7387
salimnda64-png <salimnda64@gmail.com>
2026-04-14 01:05:01 +0200

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // ================================================================
 //  CHEMINS
